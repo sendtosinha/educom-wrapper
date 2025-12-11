c645a3c362a3aca890aebcc54f17d40997614ece
sendtosinha <102919203+sendtosinha@users.noreply.github.com>
2025-12-11 09:06:18 +0530

    Add files via upload

--- android/app/src/main/java/com/edugeeks/app/MainActivity.java
+++ android/app/src/main/java/com/edugeeks/app/MainActivity.java
@@ -1,11 +1,3 @@
 package com.edugeeks.app;
-
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
